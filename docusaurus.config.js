0ba08e7d4cc5b0536930664ea1439a66b42bec95
Baur <Bauyrzhan.Yessetov@gmail.com>
2022-11-20 14:36:22 +0600

    deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Заманың түлкі болса ...',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'video, tips & tricks',
   url: 'https://github.com',
   baseUrl: '/TechBlogQZ/',
   onBrokenLinks: 'throw',
