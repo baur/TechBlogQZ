55515813f916afaf8e5a4e8d75aa81e77dd26dd1
Baur <bauyrzhan.yessetov@gmail.com>
2023-11-07 20:53:06 +0600

    doc3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,47 +1,56 @@
-// @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
-
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-
-/** @type {import('@docusaurus/types').Config} */
+import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
-  title: 'Заманың түлкі болса ...',
-  tagline: 'video, tips & tricks',
-  url: 'https://github.com',
-  baseUrl: '/TechBlogQZ/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  title: 'TechBlog',
+  tagline: 'My TechBlog Notes',
+  favicon: 'img/TechBlogLogo.png',
+
+  // Set the production url of your site here
+  url: 'https://Tech-Blog-qz.netlify.app',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'baur', // Usually your GitHub org/user name.
-  projectName: 'TechBlogQZ', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  projectName: 'TechBlog', // Usually your repo name.
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'ru',
-    locales: ['en','ru'],
+    defaultLocale: "kk",
+    locales: ["kk", "ru"],
+    localeConfigs: {
+      kk: {
+        label: "Қазақша",
+      },
+      ru: {
+        label: "Русский",
+      },
+    },
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
+          sidebarPath: "./sidebars.js",
         },
         blog: {
-          showReadingTime: false,
+          showReadingTime: true,
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: ['./src/css/custom.css'],
+        },
+        gtag: {
+          trackingID: "G-20F77MXKEW",
+          anonymizeIP: true,
         },
       }),
     ],
@@ -50,77 +59,37 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      // Replace with your project's social card
+      image: 'img/TechBlog-social-card.png',
       navbar: {
-        title: 'TechBlogQZ',
+        title: 'TechBlog',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'TechBlog',
+          src: 'img/TechBlogLogo.png',
         },
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Инструкции',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
+            type: "localeDropdown",
+            position: "left",
+          },        ],
       },
       footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        style: "dark",
+        links: [],
+        // copyright: `Arabic © <a href='https://t.me/ArabicLessonsChannel'>Сайтқа қатысты сұрақтар</a> 💬`,
+        copyright: `TechBlog`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
       },
     }),
 };
 
-module.exports = config;
+export default config;
